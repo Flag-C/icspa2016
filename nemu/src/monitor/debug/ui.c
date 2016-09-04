5465de00fece754028c579d61495d3ee076228b1
tracer-ics2016 <tracer@njuics.org>
2016-09-04 20:53:07 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-36-generic #55-Ubuntu SMP Thu Aug 11 18:01:55 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:53:07 up 10:40,  1 user,  load average: 0.33, 0.30, 0.35 50e4e7cfb883989c4aac0d1cbddadd90983c4f93

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,14 +38,34 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args);
+
+//static int cmd_info(char *args);
+
+//static int cmd_p(char *args);
+
+//static int cmd_x(char *args);
+
+//static int cmd_w(char *args);
+
+//static int cmd_d(char *args);
+
+//static int cmd_bt(char *args);
+
 static struct {
 	char *name;
 	char *description;
 	int (*handler) (char *);
 } cmd_table [] = {
 	{ "help", "Display informations about all supported commands", cmd_help },
-	{ "c", "Continue the execution of the program", cmd_c },
-	{ "q", "Exit NEMU", cmd_q },
+	{ "c", "Continue the execution of the program", cmd_c},
+	{ "q", "Exit NEMU", cmd_q},
+	{ "si", "step through", cmd_si},
+	//{ "info", "print regInfo or watchPointInfo", cmd_info},
+	//{ "x", "Scan memory", cmd_x},
+	//{ "p", "expression evaluation", cmd_p},
+	//{ "w", "set watchpoint", cmd_w},
+	//{ "d", "delete watchpoint according to the number", cmd_d},
 
 	/* TODO: Add more commands */
 
@@ -53,20 +73,36 @@ static struct {
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
 
+static int cmd_si(char* args) {
+	char *arg = strtok(NULL, " ");
+	int n;
+	if (arg == NULL)
+		cpu_exec(1);
+	else {
+		if (sscanf(arg, "%d", &n) == -1) {
+			printf("Can't read number");
+		}
+		else {
+			cpu_exec(n);
+		}
+	}
+	return 0;
+}
+
 static int cmd_help(char *args) {
 	/* extract the first argument */
 	char *arg = strtok(NULL, " ");
 	int i;
 
-	if(arg == NULL) {
+	if (arg == NULL) {
 		/* no argument given */
-		for(i = 0; i < NR_CMD; i ++) {
+		for (i = 0; i < NR_CMD; i ++) {
 			printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
 		}
 	}
 	else {
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(arg, cmd_table[i].name) == 0) {
+		for (i = 0; i < NR_CMD; i ++) {
+			if (strcmp(arg, cmd_table[i].name) == 0) {
 				printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
 				return 0;
 			}
@@ -77,19 +113,19 @@ static int cmd_help(char *args) {
 }
 
 void ui_mainloop() {
-	while(1) {
+	while (1) {
 		char *str = rl_gets();
 		char *str_end = str + strlen(str);
 
 		/* extract the first token as the command */
 		char *cmd = strtok(str, " ");
-		if(cmd == NULL) { continue; }
+		if (cmd == NULL) { continue; }
 
 		/* treat the remaining string as the arguments,
 		 * which may need further parsing
 		 */
 		char *args = cmd + strlen(cmd) + 1;
-		if(args >= str_end) {
+		if (args >= str_end) {
 			args = NULL;
 		}
 
@@ -99,13 +135,13 @@ void ui_mainloop() {
 #endif
 
 		int i;
-		for(i = 0; i < NR_CMD; i ++) {
-			if(strcmp(cmd, cmd_table[i].name) == 0) {
-				if(cmd_table[i].handler(args) < 0) { return; }
+		for (i = 0; i < NR_CMD; i ++) {
+			if (strcmp(cmd, cmd_table[i].name) == 0) {
+				if (cmd_table[i].handler(args) < 0) { return; }
 				break;
 			}
 		}
 
-		if(i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+		if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
 	}
 }
