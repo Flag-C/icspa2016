b97f0bfeb4ac9a02c71627925dbd63ceec77dbfa
151242002-Qizhi Cai <caiqizhi@smail.nju.edu.cn>
2016-09-29 21:50:21 +0800

    add the eflags

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -26,10 +26,32 @@ typedef struct {
 		/* Do NOT change the order of the GPRs' definitions. */
 		struct {
 			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+			swaddr_t eip;
+			union {
+				struct {
+					uint32_t CF: 1;
+					uint32_t : 1;
+					uint32_t PF: 1;
+					uint32_t : 1;
+					uint32_t AF: 1;
+					uint32_t : 1;
+					uint32_t ZF: 1;
+					uint32_t SF: 1;
+					uint32_t TF: 1;
+					uint32_t IF: 1;
+					uint32_t DF: 1;
+					uint32_t OF: 1;
+					uint32_t IOPL: 1;
+					uint32_t NT: 1;
+					uint32_t : 1;
+					uint32_t RF: 1;
+					uint32_t VM: 1;
+					uint32_t : 14;
+				};
+				uint32_t eflags;
+			};
 		};
 	};
-	swaddr_t eip;
-
 } CPU_state;
 
 extern CPU_state cpu;
