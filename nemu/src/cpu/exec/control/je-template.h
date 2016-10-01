c4075f409b994ff8739301060a5992125fe2976a
tracer-ics2016 <tracer@njuics.org>
2016-10-01 21:06:12 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:06:12 up  2:18,  1 user,  load average: 1.15, 1.09, 1.03 3a5aea91aa9b75be6c3dd876083210461723360

--- nemu/src/cpu/exec/control/je-template.h
+++ nemu/src/cpu/exec/control/je-template.h
@@ -2,8 +2,8 @@
 
 #define instr je
 
-make_helper (concat(je_i_, SUFFIX))
-{
+static void do_execute() {
+
 	DATA_TYPE_S displacement = op_src->val;
 	print_asm("je %x", cpu.eip + 1 + DATA_BYTE + displacement);
 	if (cpu.ZF == 1)cpu.eip += displacement;
