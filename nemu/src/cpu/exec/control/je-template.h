d39d29bb41aed635a9208811f75ed1c5148047ad
151242002-Qizhi Cai <caiqizhi@smail.nju.edu.cn>
2016-10-01 21:21:35 +0800

    add cmp

--- nemu/src/cpu/exec/control/je-template.h
+++ nemu/src/cpu/exec/control/je-template.h
@@ -3,7 +3,6 @@
 #define instr je
 
 static void do_execute() {
-
 	DATA_TYPE_S displacement = op_src->val;
 	print_asm("je %x", cpu.eip + 1 + DATA_BYTE + displacement);
 	if (cpu.ZF == 1)cpu.eip += displacement;
---
+++ nemu/src/cpu/exec/logic/cmp-template.h
@@ -0,0 +1,38 @@
+#include "cpu/exec/template-start.h"
+
+#define instr cmp
+
+static void do_execute() {
+	DATA_TYPE result = op_dest->val - op_src->val;
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF = op_dest->val < op_src->val;
+	cpu.SF = result >> len;
+	int s1, s2;
+	s1 = op_dest->val >> len;
+	s2 = op_src->val >> len;
+	cpu.OF = (s1 != s2 && s2 == cpu.SF) ;
+	cpu.ZF = !result;
+	int count = 0;
+	int i = 1;
+	for (i = 1; i <= 8; i++)
+	{
+		count += result % 2;
+		result /= 2;
+	}
+	if (count % 2 == 0)
+		cpu.PF = 1;
+	else cpu.PF = 0;
+	print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
