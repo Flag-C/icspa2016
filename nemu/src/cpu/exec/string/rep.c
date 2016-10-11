cbc9e97484d0711e623fd1741672adc636e45835
151242002-Qizhi Cai <caiqizhi@smail.nju.edu.cn>
2016-10-11 09:29:30 +0800

    modified rep

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -5,28 +5,29 @@ make_helper(exec);
 make_helper(rep) {
 	int len;
 	int count = 0;
-	if(instr_fetch(eip + 1, 1) == 0xc3) {
+	if (instr_fetch(eip + 1, 1) == 0xc3) {
 		/* repz ret */
 		exec(eip + 1);
 		len = 0;
 	}
 	else {
-		while(cpu.ecx) {
+		while (cpu.ecx) {
 			exec(eip + 1);
 			count ++;
 			cpu.ecx --;
 			assert(ops_decoded.opcode == 0xa4	// movsb
-				|| ops_decoded.opcode == 0xa5	// movsw
-				|| ops_decoded.opcode == 0xaa	// stosb
-				|| ops_decoded.opcode == 0xab	// stosw
-				|| ops_decoded.opcode == 0xa6	// cmpsb
-				|| ops_decoded.opcode == 0xa7	// cmpsw
-				|| ops_decoded.opcode == 0xae	// scasb
-				|| ops_decoded.opcode == 0xaf	// scasw
-				);
+			       || ops_decoded.opcode == 0xa5	// movsw
+			       || ops_decoded.opcode == 0xaa	// stosb
+			       || ops_decoded.opcode == 0xab	// stosw
+			       || ops_decoded.opcode == 0xa6	// cmpsb
+			       || ops_decoded.opcode == 0xa7	// cmpsw
+			       || ops_decoded.opcode == 0xae	// scasb
+			       || ops_decoded.opcode == 0xaf	// scasw
+			      );
 
 			/* TODO: Jump out of the while loop if necessary. */
-
+			if ((ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf) && cpu.ZF)
+				break;
 		}
 		len = 1;
 	}
@@ -36,24 +37,25 @@ make_helper(rep) {
 	sprintf(temp, "rep %s", assembly);
 	sprintf(assembly, "%s[cnt = %d]", temp, count);
 #endif
-	
+
 	return len + 1;
 }
 
 make_helper(repnz) {
 	int count = 0;
-	while(cpu.ecx) {
+	while (cpu.ecx) {
 		exec(eip + 1);
 		count ++;
 		cpu.ecx --;
 		assert(ops_decoded.opcode == 0xa6	// cmpsb
-				|| ops_decoded.opcode == 0xa7	// cmpsw
-				|| ops_decoded.opcode == 0xae	// scasb
-				|| ops_decoded.opcode == 0xaf	// scasw
-			  );
+		       || ops_decoded.opcode == 0xa7	// cmpsw
+		       || ops_decoded.opcode == 0xae	// scasb
+		       || ops_decoded.opcode == 0xaf	// scasw
+		      );
 
 		/* TODO: Jump out of the while loop if necessary. */
-
+		if ((ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf) && cpu.ZF)
+			break;
 	}
 
 #ifdef DEBUG
