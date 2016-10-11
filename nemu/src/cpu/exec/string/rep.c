d59f08bf587c9a2b95f2a127ede9663a935fdfb6
tracer-ics2016 <tracer@njuics.org>
2016-10-11 13:59:22 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  13:59:22 up 19:54,  1 user,  load average: 1.56, 2.04, 1.87 87e531f78bb1b35e3b55bfde60269705feeee531

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -26,7 +26,8 @@ make_helper(rep) {
 			      );
 
 			/* TODO: Jump out of the while loop if necessary. */
-			if (cpu.ecx == 0 || cpu.ZF == 0)
+			if ((ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf)
+			        && cpu.ZF == 0)
 				break;
 		}
 		len = 1;
@@ -54,7 +55,8 @@ make_helper(repnz) {
 		      );
 
 		/* TODO: Jump out of the while loop if necessary. */
-		if (cpu.ecx == 0 || cpu.ZF == 1)
+		if ((ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf)
+		        && cpu.ZF == 1)
 			break;
 	}
 
