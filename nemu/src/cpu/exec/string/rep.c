bfe5d193d5605e0649c80ec11290c55d344dee5b
tracer-ics2016 <tracer@njuics.org>
2016-10-11 09:43:26 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  09:43:26 up 15:38,  1 user,  load average: 2.30, 2.46, 2.16 af3ab119f7c02e49c4365f54105a16e9844b870

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -26,7 +26,10 @@ make_helper(rep) {
 			      );
 
 			/* TODO: Jump out of the while loop if necessary. */
-			if ((ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf) && cpu.ZF)
+			if ((ops_decoded.opcode == 0xa4 || ops_decoded.opcode == 0xa5 ||
+			        ops_decoded.opcode == 0xaa || ops_decoded.opcode == 0xab ||
+			        ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 ||
+			        ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf) && cpu.ZF)
 				break;
 		}
 		len = 1;
@@ -54,7 +57,10 @@ make_helper(repnz) {
 		      );
 
 		/* TODO: Jump out of the while loop if necessary. */
-		if ((ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 || ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf) && cpu.ZF)
+		if ((ops_decoded.opcode == 0xa4 || ops_decoded.opcode == 0xa5 ||
+		        ops_decoded.opcode == 0xaa || ops_decoded.opcode == 0xab ||
+		        ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7 ||
+		        ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf) && cpu.ZF)
 			break;
 	}
 
