a676ff6335bef4e728779e06cf13f2ff1d62bcd4
tracer-ics2016 <tracer@njuics.org>
2016-10-15 21:42:50 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:42:50 up  3:31,  1 user,  load average: 1.18, 1.17, 1.17 11a3c7741d3d80089263dce2d183fd4270ee5dcd

--- nemu/src/cpu/exec/data-mov/cwd-template.h
+++ nemu/src/cpu/exec/data-mov/cwd-template.h
@@ -5,7 +5,7 @@
 make_helper(concat(cwd_, SUFFIX)) {
 	DATA_TYPE temp = REG(R_EAX);
 	int len = (DATA_BYTE << 3) - 1;
-	REG(R_EDX) = temp >> len;
+	REG(R_EDX) = temp >> (len + 1);
 	print_asm("cwd");
 	return 1;
 }
