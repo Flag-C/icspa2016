b9ad310440f624d07d09c71a47b7378e226e253f
151242002-Qizhi Cai <caiqizhi@smail.nju.edu.cn>
2016-10-17 00:37:15 +0800

    finished print var name, considering the last one in the strtable is significant.

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -233,13 +233,13 @@ uint32_t eval(int p, int q) {
 				{
 					char name[32];
 					int len = symtab[i + 1].st_name - symtab[i].st_name - 1;
-					Log("len=%d\n", len);
+					//Log("len=%d\n", len);
 					strncpy(name, strtab + symtab[i].st_name, len);
 					name[len] = '\0';
 					if (strcmp(tokens[p].str, name) == 0)
 						num = symtab[i].st_value;
 				}
-			if ((symtab[i].st_info & 0xf) == STT_OBJECT)
+			if ((symtab[i].st_info & 0xf) == STT_OBJECT) //we must consider the last one in the stringtable.
 			{
 				char name[32];
 				strcpy(name, strtab + symtab[i].st_name);
