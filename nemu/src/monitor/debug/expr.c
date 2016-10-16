88148940a0e6aeff0f9496ce91c710dca2cbaabd
tracer-ics2016 <tracer@njuics.org>
2016-10-17 00:06:53 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  00:06:53 up 1 day,  5:55,  1 user,  load average: 1.15, 1.08, 1.02 4ec95834e5dd6bf489716eef4f56d3a2c69847aa

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -231,10 +231,10 @@ uint32_t eval(int p, int q) {
 			for (i = 0; i < nr_symtab_entry; i++)
 				if ((symtab[i].st_info & 0xf) == STT_OBJECT)
 				{
-					char name[32];
+					char* name;
 					int len = symtab[i + 1].st_name - symtab[i].st_name - 1;
 					strncpy(name, strtab + symtab[i].st_name, len);
-					name[len] = '\0';
+					strcat(name, "\0");
 					if (strcmp(tokens[p].str, name) == 0)
 						num = symtab[i].st_value;
 				}
