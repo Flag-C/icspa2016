072dac67b25db53c1412a04e27a81c7ddc933514
tracer-ics2016 <tracer@njuics.org>
2016-10-21 22:51:07 +0800

    >  run 151242002 soulwing Linux soulwing-CP65S 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:51:07 up 1 day,  1:57,  1 user,  load average: 1.30, 1.18, 1.13 9e326a3bdd900f45d068ada66bc8d14e4dd4e0a1

--- testcase/src/print-FLOAT.c
+++ testcase/src/print-FLOAT.c
@@ -14,14 +14,16 @@ int main() {
 	printf("%f\n", FLOAT_ARG(0x00013333));
 	printf("%f %d\n", FLOAT_ARG(0xfffecccd), 123456);
 #else
-	sprintf(buf, "%d", 123456);
+	printf(buf, "%d", 123456);
+	nemu_assert(strcmp(buf, "123456") == 0);
+	/*sprintf(buf, "%f", FLOAT_ARG(0x00010000));
 	nemu_assert(strcmp(buf, "1.000000") == 0);
 
 	sprintf(buf, "%f", FLOAT_ARG(0x00013333));
 	nemu_assert(strcmp(buf, "1.199996") == 0);
 
 	sprintf(buf, "%f %d", FLOAT_ARG(0xfffecccd), 123456);
-	nemu_assert(strcmp(buf, "-1.199996 123456") == 0);
+	nemu_assert(strcmp(buf, "-1.199996 123456") == 0);*/
 #endif
 
 	return 0;
