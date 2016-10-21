6e14d935fc0ac9f96fa21e553edf6d405d3b5ca4
tracer-ics2016 <tracer@njuics.org>
2016-10-21 22:05:44 +0800

    >  run 151242002 soulwing Linux soulwing-CP65S 4.4.0-38-generic #57-Ubuntu SMP Tue Sep 6 15:42:33 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  22:05:44 up 1 day,  1:12,  1 user,  load average: 1.13, 1.06, 1.16 25db72712fcf5207419f5b801dfffedd934ea38b

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -112,10 +112,10 @@ static void modify_vfprintf() {
 		}
 
 		static void modify_ppfs_setargs() {
-			//char* add = &_ppfs_setargs + 0x801114 - 0x8010a5;
-			//mprotect((void *)((int)((int*)add) & 0xfffff000), 4096 * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
-			//*add = 0xeb;
-			//*(add + 1) = 0xe4;
+			char* add = &_ppfs_setargs + 0x801114 - 0x8010a5;
+			mprotect((void *)((int)((int*)add) & 0xfffff000), 4096 * 2, PROT_READ | PROT_WRITE | PROT_EXEC);
+			*add = 0xeb;
+			*(add + 1) = (char)(0x801167 - (char)&_ppfs_setargs);
 			/* TODO: Implement this function to modify the action of preparing
 			 * "%f" arguments for _vfprintf_internal() in _ppfs_setargs().
 			 * Below is the code section in _vfprintf_internal() relative to
