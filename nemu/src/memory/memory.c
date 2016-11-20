be9db7c99882dcd43fc61559d405caa0d546710a
tracer-ics2016 <tracer@njuics.org>
2016-11-20 18:52:13 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:52:13 up  8:29,  1 user,  load average: 0.92, 0.98, 1.02 d4e17f392e060fd097412c756d5ca620e677caa4

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 #define CACHED
+#define DEBUGGING
 
 extern void* l1_cache_interface;
 
@@ -14,16 +15,22 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
+#ifdef DEBUGGING
+	printf("read addr:%x\n", addr);
+#endif
 #ifndef CACHED
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 #endif
 
 #ifdef CACHED
-	return L1_read(l1_cache_interface, addr, len);
+	return L1_read(l1_cache_interface, addr, len) & (~0u >> ((4 - len) << 3));
 #endif
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
+#ifdef DEBUGGING
+	printf("write addr:%x data:%x\n", addr, data);
+#endif
 #ifndef CACHED
 	dram_write(addr, len, data);
 #endif
