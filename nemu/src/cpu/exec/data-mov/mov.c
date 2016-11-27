5e7a5f5188df14add54563b2f3048b080cb5f043
tracer-ics2016 <tracer@njuics.org>
2016-11-27 17:07:01 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:07:01 up 17:36,  1 user,  load average: 1.52, 1.29, 1.19 222a2d823eeaa06561de6710f7e5c850a094a8e2

--- nemu/src/cpu/exec/data-mov/mov.c
+++ nemu/src/cpu/exec/data-mov/mov.c
@@ -20,3 +20,25 @@ make_helper_v(mov_r2rm)
 make_helper_v(mov_rm2r)
 make_helper_v(mov_a2moffs)
 make_helper_v(mov_moffs2a)
+
+make_helper(mov_crx2r)
+{
+	uint8_t registers = instr_fetch(eip + 1, 1);
+	int dest = registers & 0x7;
+	int src = (registers >> 3) & 0x7;
+	if (src == 0)
+		reg_l(dest) = cpu.cr0.val;
+	else Assert(1, "Not accomplish such cr reg");
+	return 2;
+}
+
+make_helper(mov_r2crx)
+{
+	uint8_t registers = instr_fetch(eip + 1, 1);
+	int src = registers & 0x7;
+	int dest = (registers >> 3) & 0x7;
+	if (dest == 0)
+		cpu.cr0.val = reg_l(src);
+	else Assert(1, "Not accomplish such cr reg");
+	return 2;
+}
