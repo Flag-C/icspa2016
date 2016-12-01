39f2cc304e5a2e780ea40a0b3c801b4166129947
tracer-ics2016 <tracer@njuics.org>
2016-12-02 02:00:03 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  02:00:03 up  1:30,  1 user,  load average: 0.18, 0.14, 0.19 c5df7c11d593e02f63bbd5f49338f8f75bed7067

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -55,9 +55,11 @@ hwaddr_t page_translate(lnaddr_t addr)
 	hwaddr_t dir_addr = (cpu.cr3.page_directory_base << 12) + dir * 4;
 	PDE page_dir;
 	page_dir.val = hwaddr_read(dir_addr, 4);
+	Log("dir_addr=%x", dir_addr);
 	Assert(page_dir.present == 1, "unvalid page directry");
 	hwaddr_t tab_addr = (page_dir.page_frame << 12) + page * 4;
 	PTE page_tab;
+	Log("tab addr=%x", tab_addr);
 	page_tab.val = hwaddr_read(tab_addr, 4);
 	Assert(page_tab.present == 1, "unvalid page table");
 	return (page_tab.page_frame << 12) + offset;
