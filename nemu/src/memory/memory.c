4e72c7e8b9842804de51b516a58297aff3ad8198
tracer-ics2016 <tracer@njuics.org>
2016-12-02 23:13:39 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  23:13:39 up 22:43,  1 user,  load average: 1.15, 1.15, 1.16 cc95c37691dde70b70727083147660a9cef6899

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -106,8 +106,18 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
 	if ((cpu.cr0.protect_enable == 1) && (cpu.cr0.paging == 1))
 	{
-		if ((addr & 0xfffff000) != ((addr + len - 1) & 0xfffff000))
-			Assert(0, "read cross page");
+		if ((addr & 0xfffff000) != ((addr + len - 1) & 0xfffff000)) {
+			//read cross page
+			lnaddr_t prev_page_addr = addr & 0xfffff000;
+			lnaddr_t sec_page_addr = (addr + len - 1) & 0xfffff000;
+			uint32_t prev_len = 4096 - (addr - prev_page_addr);
+			uint32_t sec_len = len - prev_len;
+			Log("read cross page,lnaddr=%x,hwaddr=%x", addr,
+			    (hwaddr_read(page_translate(sec_page_addr), sec_len) << prev_len)
+			    + hwaddr_read(page_translate(prev_page_addr), prev_len));
+			return (hwaddr_read(page_translate(sec_page_addr), sec_len) << prev_len)
+			       + hwaddr_read(page_translate(prev_page_addr), prev_len);
+		}
 		else {
 			//Log("addr to be translated:%x", addr);
 			hwaddr_t hwaddr = page_translate(addr);
