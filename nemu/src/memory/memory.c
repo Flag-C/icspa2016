13266b0c78cd8e3ed407c3513a91e5962c697a1f
tracer-ics2016 <tracer@njuics.org>
2016-12-02 23:18:10 +0800

    >  compile NEMU 151242002 soulwing Linux soulwing-CP65S 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  23:18:10 up 22:48,  1 user,  load average: 1.19, 1.20, 1.18 2e23e4934557067f216daf4d19b4b1074f274ffa

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -112,9 +112,7 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 			lnaddr_t sec_page_addr = (addr + len - 1) & 0xfffff000;
 			uint32_t prev_len = 4096 - (addr - prev_page_addr);
 			uint32_t sec_len = len - prev_len;
-			Log("read cross page,lnaddr=%x,hwaddr=%x", addr,
-			    (hwaddr_read(page_translate(sec_page_addr), sec_len) << prev_len)
-			    + hwaddr_read(page_translate(prev_page_addr), prev_len));
+			Log("read cross page,lnaddr=%x,hwaddr1=%x,hwaddr2=%x", addr, page_translate(prev_page_addr), page_translate(sec_page_addr));
 			return (hwaddr_read(page_translate(sec_page_addr), sec_len) << prev_len)
 			       + hwaddr_read(page_translate(prev_page_addr), prev_len);
 		}
