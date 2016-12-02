6b35e6be6700cb1e2ace63d1e485e04f155615a8
tracer-ics2016 <tracer@njuics.org>
2016-12-02 18:30:00 +0800

    >  compile kernel 151242002 soulwing Linux soulwing-CP65S 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:30:00 up 18:00,  1 user,  load average: 0.91, 1.14, 1.10 b1c2389a8b413e9fcb85d474cc8e0dcce22f6115

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -17,12 +17,12 @@ void create_video_mapping() {
 	 */
 	//panic("please implement me");
 	uint32_t addr = VMEM_ADDR;
-	int pages = SCR_SIZE / 4096;
+	int pages = SCR_SIZE / 4096 + 1;
 	int i;
 	for (i = 0; i < pages; i++)
 	{
-		unsigned dir = 0x3ff & (addr >> 22);
-		unsigned page = 0x3ff & (addr >> 12);
+		uint32_t dir = 0x3ff & (addr >> 22);
+		uint32_t page = 0x3ff & (addr >> 12);
 		PTE *pt = kptable[0];
 		(get_updir() + dir)->val = make_pde(va_to_pa(pt));
 		pt[page].val = make_pte(addr);
