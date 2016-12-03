f9bc176006c4f07efd6a0289fd934b8425ffeb55
tracer-ics2016 <tracer@njuics.org>
2016-12-03 14:42:15 +0800

    >  compile kernel 151242002 soulwing Linux soulwing-CP65S 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:42:15 up 1 day, 14:12,  1 user,  load average: 0.88, 0.61, 0.70 bc459411ea5f5bb221943248da5e845c192c28ef

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -54,10 +54,10 @@ void init_cond() {
 
 	/* Initialize the serial port. After that, you can use printk() to output messages. */
 	init_serial();
-	Log("TEST");
 
 	/* Initialize the IDE driver. */
 	init_ide();
+	Log("TEST");
 
 	/* Enable interrupts. */
 	sti();
