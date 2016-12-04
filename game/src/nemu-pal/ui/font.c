dec1ec7c65712af5e7583f9ae6f36147c5ed1f7c
tracer-ics2016 <tracer@njuics.org>
2016-12-04 17:16:51 +0800

    >  compile game 151242002 soulwing Linux soulwing-CP65S 4.4.0-47-generic #68-Ubuntu SMP Wed Oct 26 19:39:52 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:16:51 up 2 days, 16:47,  1 user,  load average: 1.16, 1.25, 1.26 dcf59ffbdb65e73fe222537ab489d600fb19d4c5

--- game/src/nemu-pal/ui/font.c
+++ game/src/nemu-pal/ui/font.c
@@ -410,7 +410,9 @@ PAL_InitFont(
   // Read all bitmaps from wor16.fon file.
   //
   fp = UTIL_OpenRequiredFile("wor16.fon");
+  Log("hit");
   gpFont->lpBufGlyph = (LPBYTE)calloc(gpFont->nChar, 30);
+  Log("leave");
   if (gpFont->lpBufGlyph == NULL)
   {
     free(gpFont->lpBufChar);
